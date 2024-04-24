e8fc198fae5437ec25112832869ed5353873b983
dotoritoring <khylove0503@sju.ac.kr>
2024-04-24 19:03:01 +0900

    chore : 게임방 crud 로직 수정

--- controller/Cgame.js
+++ controller/Cgame.js
@@ -1,15 +1,23 @@
+const { where } = require("sequelize");
 const { Game } = require("../models");
 
 // 게임방 생성
 // post /games
 exports.postGame = async (req, res) => {
     const { title, pw, type } = req.body;
+    var state = 1;
+    if (pw) {
+        // 비밀번호가 있을 경우, 진입 불가
+        state = 0;
+    }
     try {
         await Game.create({
             g_seq: null,
             g_title: title,
             g_pw: pw,
             g_type: type,
+            g_total: 1,
+            g_state: state,
         });
         res.send(true);
     } catch {
@@ -35,8 +43,8 @@ exports.getGame = async (req, res) => {
 };
 
 // 게임방 설정 변경
-// patch /games/:g_seq
-exports.patchGame = async (req, res) => {
+// patch /games/setting/:g_seq
+exports.patchGameSetting = async (req, res) => {
     const { g_seq } = req.params;
     const { title, pw } = req.body;
 
@@ -55,6 +63,79 @@ exports.patchGame = async (req, res) => {
         res.status(500).send("server error");
     }
 };
+
+// 게임방 인원 증가
+// patch /games/plus/:g_seq
+exports.patchPlus = async (req, res) => {
+    const { g_seq } = req.params;
+    try {
+        const gameInfo = await Game.findOne({
+            where: { g_seq },
+        });
+        const new_total = gameInfo.g_total + 1;
+
+        if (gameInfo.g_type) {
+            // 마피아
+            if (new_total > 8) res.send("인원이 초과되어 입장이 불가능합니다.");
+            else {
+                await Game.update(
+                    {
+                        g_total: new_total,
+                    },
+                    {
+                        where: { g_seq },
+                    }
+                );
+                res.send(true);
+            }
+        } else {
+            // 캐치 라이어
+            if (new_total > 6) res.send("인원이 초과되어 입장이 불가능합니다.");
+            else {
+                await Game.update(
+                    {
+                        g_total: new_total,
+                    },
+                    {
+                        where: { g_seq },
+                    }
+                );
+                res.send(true);
+            }
+        }
+    } catch {
+        res.status(500).send("server error");
+    }
+};
+
+// 게임방 인원 감소
+// patch /games/minus/:g_seq
+exports.patchMinus = async (req, res) => {
+    const { g_seq } = req.params;
+    try {
+        const gameInfo = await Game.findOne({
+            where: { g_seq },
+        });
+        const new_total = gameInfo.g_total - 1;
+
+        if (new_total <= 0) {
+            res.send(false);
+        } else {
+            await Game.update(
+                {
+                    g_total: new_total,
+                },
+                {
+                    where: { g_seq },
+                }
+            );
+            res.send(true);
+        }
+    } catch {
+        res.status(500).send("server error");
+    }
+};
+
 // 게임방 삭제
 exports.deleteGame = async (req, res) => {
     const { g_seq } = req.params;
